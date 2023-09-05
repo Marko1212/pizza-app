0fee43b739a2bdb93863dcc4cb7f204f108ec694
Marko_Askovic <marko.askovic@casasoft.com>
2023-09-05 16:19:57 +0200

    small correction

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -14,6 +14,7 @@ export function Menu() {
 	const getMenu = async () => {
 		try {
 			setIsLoading(true);
+			setError(undefined);
 			await new Promise<void>((resolve) => {
 				setTimeout(() => {
 					resolve();
