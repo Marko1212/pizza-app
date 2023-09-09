3ff9f68fa1720d633404e9843dddc9d18ac73a16
Marko_Askovic <marko.askovic@casasoft.com>
2023-09-09 15:11:34 +0200

    avec useLocation

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -51,3 +51,59 @@ export function Layout() {
 		</div>
 	);
 }
+
+/* Version avec le hook useLocation
+import { Link, NavLink, Outlet, useLocation } from 'react-router-dom';
+import styles from './Layout.module.css';
+import Button from '../../components/Button/Button';
+import { useEffect } from 'react';
+import cn from 'classnames';
+
+export function Layout() {
+	const location = useLocation();
+
+	useEffect(() => {
+		console.log(location);
+	}, [location]);
+
+	return (
+		<div className={styles['layout']}>
+			<div className={styles.sidebar}>
+				<div className={styles.user}>
+					<img className={styles.avatar} src='/avatar.png' alt='avatar' />
+					<div className={styles.name}>Victor Hugo</div>
+					<div className={styles.email}>victor@hugo.fr</div>
+				</div>
+				<div className={styles.menu}>
+					<NavLink
+						to='/'
+						className={({ isActive }) =>
+							cn(styles.link, {
+								[styles.active]: isActive
+							})
+						}
+					>
+						<img src='/menu-icon.svg' alt='icône du menu' />
+						Menu
+					</NavLink>
+					<NavLink
+						to='/cart'
+						className={cn(styles.link, {
+							[styles.active]: location.pathname === '/cart'
+						})}
+					>
+						<img src='/cart-icon.svg' alt='icône de la corbeille' />
+						Cart
+					</NavLink>
+				</div>
+				<Button className={styles.exit}>
+					<img src='/exit-icon.svg' alt='Icône de la sortie' />
+					Sortie
+				</Button>
+			</div>
+			<div className={styles.content}>
+				<Outlet />
+			</div>
+		</div>
+	);
+} */
